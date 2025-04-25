56ac7f0ab747bfe6c6b86083641a4cdf5eab3944
Marcos Venicios E. Chaves <markusvynycyus@hotmail.com>
2025-04-25 11:00:10 -0300

    implementar CreditoService com métodos buscarPorNumeroNfse e buscarPorNumeroCredito, utilizando CreditoRepository e CreditoModelAssembler

---
+++ src/main/java/com/venicios/api_credito_nfse/service/CreditoService.java
@@ -0,0 +1,32 @@
+package com.venicios.api_credito_nfse.service;
+
+import com.venicios.api_credito_nfse.assembler.CreditoModelAssembler;
+import com.venicios.api_credito_nfse.dto.CreditoDTO;
+import com.venicios.api_credito_nfse.model.Credito;
+import com.venicios.api_credito_nfse.repository.CreditoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CreditoService {
+
+    private final CreditoRepository repository;
+    private final CreditoModelAssembler assembler;
+
+    @Autowired
+    public CreditoService(CreditoRepository repository, CreditoModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+    public List<CreditoDTO> buscarPorNumeroNfse(String numeroNfse) {
+        List<Credito> creditos = repository.findByNumeroNfse(numeroNfse);
+        return assembler.toCollectionModel(creditos);
+    }
+    public List<CreditoDTO> buscarPorNumeroCredito(String numeroCredito) {
+        List<Credito> creditos = repository.findByNumeroCredito(numeroCredito);
+        return assembler.toCollectionModel(creditos);
+    }
+}
