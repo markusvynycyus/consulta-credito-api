f5c2c78ec73117e92c79dbaf6576f59c17b5f82c
Marcos Venicios E. Chaves <markusvynycyus@hotmail.com>
2025-04-25 09:42:38 -0300

    Inciando a construcao do projeto

---
+++ src/main/java/com/venicios/api_credito_nfse/ApiCreditoNfseApplication.java
@@ -0,0 +1,13 @@
+package com.venicios.api_credito_nfse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCreditoNfseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCreditoNfseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venicios/api_credito_nfse/ApiCreditoNfseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venicios.api_credito_nfse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCreditoNfseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
